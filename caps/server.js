728c4a5bed1aea4bb1637858c1e7f714c9db9768
RivaD2 <rivkadavidowski@gmail.com>
2020-11-04 19:53:31 -0800

    refactored again for socket io, server running, vendor not connected

--- caps/server.js
+++ caps/server.js
@@ -4,61 +4,34 @@ require('dotenv').config();
 const port = process.env.PORT || 3001;
 
 //creating server and namespaces
-const server = require('net').createServer();
-const io = require('socket.io')(server);
+const io = require('socket.io');
+const server = io(port);
 
+const caps = server.of('/caps');
 let clients = [];
 // My server is a place where connections can be made
 // I can specify how many connections I want to have
 // Anyone who wants to connect has to have the port #
-// Socket is a client
-    io.on('connection', socket => {
+// Socket is a clients
+    server.on('connection', socket => {
+        console.log('server is on!')
         clients.push(socket);
-       io.emit('data', data => {
+       server.on('data', data => {
             const message = JSON.parse(data.toString())
             console.log(message);
             if(message.event == 'pickup') {
                 console.log('Pickup needed!');
-                socket.broadcast.emit(data);
+                caps.broadcast.emit(data);
             }
             if(message.event == 'delivered') {
                 console.log('order delivered!');
-                socket.broadcast.emit(data);
+                caps.emit(data);
             }
         })
     })
 
-let rooms = [];
-class Room {
-    constructor(name, io) {
-      this.name = name;
-      this.users = [];
-      this.namespace = io.of('/caps' + name);
-      this.listenOnRoom();
-    }
 
-    listenOnRoom() {
-      this.namespace.on('connection', (socket) => {
-        
-  
-        socket.on('disconnect', (message) => {
-         console.log(name + 'disconnected');
-        });
-  
-        socket.on('pickup', data => {
-      
-        });
-        socket.on('in-transit', data => {
-      
-        });
-        socket.on('delivered', data => {
-      
-        });
-      });
-    }
-  }
-
-io.broadcast = data => {
+caps.broadcast = data => {
     //sending message object from vendor
     // This message is sent to every client
     clients.forEach(socket => {
@@ -66,7 +39,3 @@ io.broadcast = data => {
     })
 }
 
-server.listen(port, () => {  
-  console.log(`server listening on port ${port}`);
-});
-module.exports = Room;
