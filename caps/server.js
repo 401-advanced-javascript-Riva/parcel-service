5c262a67bd80c635f003d68e65084f6c06f2c57b
RivaD2 <rivkadavidowski@gmail.com>
2020-11-05 18:02:25 -0800

    added notes for code for queue

--- caps/server.js
+++ caps/server.js
@@ -35,4 +35,59 @@ driver.on('connection', socket => {
 })
 
 
+const queue = {
+    pickup: {},
+    delivered: {},
+    inTransit: {}
+}
 
+server.on('connection', socket => {
+    socket.on('pickup', payload => {
+        //add payload to inTransit object
+        let id = Math.floor(Math.random() + 10000000);
+
+        queue.inTransit(id) = payload;
+        socket.broadcast.emit('pick-up', {id, payload})
+    })
+    socket.on('in-transit', order => {
+        let {id, payload} = order;
+        delete queue.pickup[id];
+        queue.inTransit[id] = payload;
+        console.log('this is the queue', queue)
+        socket.broadcast.emit('in-transit', payload)
+    })
+    socket.on('delivered', payload => {
+
+    })
+})
+/*in vendor.js:
+setInterval(() => {
+    //faker stuff inside
+    console.log('sending order for pickup');
+    socket.emit('pickup', {
+    address:
+    phone:
+})
+}, 500)*/
+
+
+/*
+
+socket.on('getPickups', () => {
+    Object.keys(queue.pickup). forEach(id => {
+        //fetch all puickups order in queue
+        //order is organized by id and I need 
+        socket.emit('pickup', {id, payload: queue.pickup[id]})
+    })
+})
+*/
+/*
+in driver.js:
+const socket = io.connect('localhost)
+socket.emit('getPickups');
+//for every items in pickups, I have a different emission
+socket.on('pickup', order => {
+    console.log('incoming order', order);
+    socket.emit('in-transit', order);
+});
+*/
